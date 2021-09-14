e0baafff65c3638a2525822e5cc1892bd1256748
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-14 16:46:10 +1000

    Add WIP treeview demo

---
+++ TreeViewManually/TreeViewManually/Data/Node.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TreeViewManually.Data
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public List<Node> Children { get; set; }
+    }
+}
