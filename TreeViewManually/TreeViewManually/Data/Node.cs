2dc2c74828406ebd847f7083a213abbe1b4bb3a2
anddrzejb <anddrzejb@poczta.fm>
2021-09-14 12:18:09 +0200

    select/deselect feature

--- TreeViewManually/TreeViewManually/Data/Node.cs
+++ TreeViewManually/TreeViewManually/Data/Node.cs
@@ -1,10 +1,79 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TreeViewManually.Data
 {
     public class Node
     {
+        public string Key { get; set; } = Guid.NewGuid().ToString();
         public string Value { get; set; }
         public List<Node> Children { get; set; }
     }
+
+    public static class TreeView 
+    {
+        public static List<Node> SampleData = new()
+        {
+            new Node()
+            {
+                Value = "Shops",
+                Children = new List<Node>()
+                {
+                    new Node()
+                    {
+                        Value = "Grocery Store",
+
+                    },
+                    new Node()
+                    {
+                        Value = "DIY",
+
+                    },
+                    new Node()
+                    {
+                        Value = "Supermarket",
+                        Children = new List<Node>()
+                        {
+                            new Node
+                            {
+                                Value = "Fishing Department"
+                            },
+                            new Node
+                            {
+                                Value = "Sports Department"
+                            }
+                        }
+                    },
+                    new Node()
+                    {
+                        Value = "Others",
+
+                    },
+                }
+            },
+            new Node()
+            {
+                Value = "Services",
+                Children = new List<Node>()
+                {
+                    new Node()
+                    {
+                        Value = "Barber",
+
+                    },
+                    new Node()
+                    {
+                        Value = "Repair Shop",
+
+                    },
+                    new Node()
+                    {
+                        Value = "Others",
+
+                    },
+                }
+            },
+        };
+    }
+
 }
--- TreeViewManually/TreeViewManually/Startup.cs
+++ TreeViewManually/TreeViewManually/Startup.cs
@@ -29,6 +29,7 @@ namespace TreeViewManually
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddAntDesign();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
