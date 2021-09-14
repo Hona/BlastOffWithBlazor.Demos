66cf62847299732ee1df7447247f864965c8260d
anddrzejb <anddrzejb@poczta.fm>
2021-09-14 14:30:26 +0200

    notification with custom button to clear log

--- TreeViewManually/TreeViewManually/Startup.cs
+++ TreeViewManually/TreeViewManually/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FindRazorSourceFile.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -38,6 +39,8 @@ namespace TreeViewManually
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseFindRazorSourceFile();
             }
             else
             {
