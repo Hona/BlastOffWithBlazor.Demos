4139e4f049b164bef5b70e09ef7b4e6ffb7f99ac
Luke Parker <10430890+Hona@users.noreply.github.com>
2021-09-15 20:34:22 +1000

    End of user group

---
+++ AntBlazorWeatherForecast/Program.cs
@@ -0,0 +1,26 @@
+using AntDesign.ProLayout;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AntBlazorWeatherForecast.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntBlazorWeatherForecast
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+            builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
